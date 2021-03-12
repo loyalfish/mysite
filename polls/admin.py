b17fa8f80fc52bb4f8afe0cca13411e9e2a87b89
loyalfish <70802120+loyalfish@users.noreply.github.com>
2021-03-13 00:27:54 +0700

    Add files via upload

---
+++ polls/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+
+from .models import Question, Choice
+
+class ChoiceAdmin(admin.StackedInline):
+    fieldsets = [
+        ('Вариант ответа' , {"fields" : ["text", "votes"]}),
+    ]
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Текст вопроса' , {"fields" : ["text"]}),
+        ('Дата вопроса' , {"fields" : ["pub_date"]}),
+    ]
+    inlines = [ChoiceAdmin]
+    list_filter = ["pub_date"]
+    search_fields = ["text"]
+
+admin.site.register(Question, QuestionAdmin)
+
+
