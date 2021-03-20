206e3c0a5cc5b51f99d6295272be2f5891f647aa
twic69 <gaming.from.integral.2k18@gmail.com>
2021-03-20 15:48:16 +0300

    add

--- polls/admin.py
+++ polls/admin.py
@@ -1,4 +1,23 @@
 from django.contrib import admin
 
-from .models import Question
-admin.site.register(Question)
+from .models import Question, Choice
+
+class ChoiceAdmin(admin.TabularInline):
+    fieldsets = [
+        ("Вариант ответа", {"fields" : ["text", "votes"]} ),
+    ]
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ("Текст вопроса", {"fields" : ["text"]} ),
+        ("Дата создания вопроса", {"fields" : ["pub_date"]} ),
+
+    ]
+    inlines = [ChoiceAdmin]
+    list_filter = ["pub_date"]
+    search_fields = ["text"]
+
+
+admin.site.register(Question, QuestionAdmin)
--- polls/views.py
+++ polls/views.py
@@ -1,28 +1,38 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.views import generic
 from .models import Question, Choice
 # Create your views here.
-def index(request):
-    q_all = Question.objects.all()
-    res = "<col>"
-    for q in q_all:
-        res += "<li>%s</li>" % q.text
-    res += "</ol>"
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "questions"
+    def get_queryset(self):
+        return Question.objects.all()
+        
 
-    return HttpResponse(res)
-    
-def detail(request, q_id):
-    res = "Question number %s." % q_id
-    return HttpResponse(res)
 
-def results(request, q_id):
-    res = "Result for question number %s." % q_id
-    return HttpResponse(res)
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
 
 def vote(request, q_id):
-    res = "Vote for question number %s." % q_id
-    return HttpResponse(res)
+    choices = request.POST.getlist("choice")
+    question = Question.objects.get(pk=q_id)
+
+    for choice_pk in choices:
+        choice = question.choice_set.get(pk=choice_pk)
+        choice.votes += 1
+        choice.save()
+
+    return HttpResponseRedirect( reverse("polls:result", args=(q_id, )) )
+
 
 def meme(request):
     return HttpResponse('<img src="https://n1s1.hsmedia.ru/c8/9f/cb/c89fcb31dd077084bc8bbc2284586b7f/1000x600_0xac120003_16946826431608901545.jpg"> ')
